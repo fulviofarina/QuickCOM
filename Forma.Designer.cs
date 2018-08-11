cd3b1345f1782ac7fc31b531ec0769c84b9a0092
Fulvio Farina <fulviofarina@gmail.com>
2018-08-11 14:06:31 -0400

    Initial commit

---
+++ Forma.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace QuickCOM
+{
+    partial class Forma
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.transmitterRTB = new System.Windows.Forms.RichTextBox();
+            this.receiverRTB = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.clearBtn1 = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.clearBtn2 = new System.Windows.Forms.Button();
+            this.sendFileBtn = new System.Windows.Forms.Button();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.resetHostBtn = new System.Windows.Forms.Button();
+            this.ping = new System.Windows.Forms.Button();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.ucScanner1 = new VTools.ucScanner();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.Panel2.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // transmitterRTB
+            // 
+            this.transmitterRTB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.transmitterRTB.Location = new System.Drawing.Point(3, 33);
+            this.transmitterRTB.Name = "transmitterRTB";
+            this.transmitterRTB.Size = new System.Drawing.Size(834, 178);
+            this.transmitterRTB.TabIndex = 1;
+            this.transmitterRTB.Text = "";
+            // 
+            // receiverRTB
+            // 
+            this.receiverRTB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.receiverRTB.Location = new System.Drawing.Point(3, 32);
+            this.receiverRTB.Name = "receiverRTB";
+            this.receiverRTB.Size = new System.Drawing.Size(832, 180);
+            this.receiverRTB.TabIndex = 2;
+            this.receiverRTB.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // clearBtn1
+            // 
+            this.clearBtn1.Location = new System.Drawing.Point(843, 3);
+            this.clearBtn1.Name = "clearBtn1";
+            this.clearBtn1.Size = new System.Drawing.Size(75, 23);
+            this.clearBtn1.TabIndex = 4;
+            this.clearBtn1.Text = "Clear";
+            this.clearBtn1.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tableLayoutPanel1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tableLayoutPanel2);
+            this.splitContainer1.Size = new System.Drawing.Size(949, 475);
+            this.splitContainer1.SplitterDistance = 235;
+            this.splitContainer1.TabIndex = 5;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 88.52631F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.47368F));
+            this.tableLayoutPanel1.Controls.Add(this.transmitterRTB, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.clearBtn1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.35185F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 85.64815F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(949, 235);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(834, 30);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "SEND BOX";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 88.31579F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.68421F));
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.clearBtn2, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.receiverRTB, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13.61502F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 86.38498F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(949, 236);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(832, 29);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "RECEIVE BOX";
+            // 
+            // clearBtn2
+            // 
+            this.clearBtn2.Location = new System.Drawing.Point(841, 3);
+            this.clearBtn2.Name = "clearBtn2";
+            this.clearBtn2.Size = new System.Drawing.Size(75, 23);
+            this.clearBtn2.TabIndex = 4;
+            this.clearBtn2.Text = "Clear";
+            this.clearBtn2.UseVisualStyleBackColor = true;
+            // 
+            // sendFileBtn
+            // 
+            this.sendFileBtn.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sendFileBtn.Location = new System.Drawing.Point(3, 91);
+            this.sendFileBtn.Name = "sendFileBtn";
+            this.sendFileBtn.Size = new System.Drawing.Size(94, 23);
+            this.sendFileBtn.TabIndex = 6;
+            this.sendFileBtn.Text = "SEND FILE";
+            this.sendFileBtn.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.splitContainer1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.tableLayoutPanel3);
+            this.splitContainer2.Size = new System.Drawing.Size(1106, 475);
+            this.splitContainer2.SplitterDistance = 949;
+            this.splitContainer2.TabIndex = 7;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.resetHostBtn, 0, 3);
+            this.tableLayoutPanel3.Controls.Add(this.ping, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.sendFileBtn, 0, 3);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 4;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 344F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 43F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(153, 475);
+            this.tableLayoutPanel3.TabIndex = 7;
+            // 
+            // resetHostBtn
+            // 
+            this.resetHostBtn.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetHostBtn.Location = new System.Drawing.Point(3, 435);
+            this.resetHostBtn.Name = "resetHostBtn";
+            this.resetHostBtn.Size = new System.Drawing.Size(94, 23);
+            this.resetHostBtn.TabIndex = 9;
+            this.resetHostBtn.Text = "RESET HOST";
+            this.resetHostBtn.UseVisualStyleBackColor = true;
+            // 
+            // ping
+            // 
+            this.ping.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ping.Location = new System.Drawing.Point(3, 3);
+            this.ping.Name = "ping";
+            this.ping.Size = new System.Drawing.Size(94, 22);
+            this.ping.TabIndex = 7;
+            this.ping.Text = "PING";
+            this.ping.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            this.splitContainer3.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.ucScanner1);
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer3.Size = new System.Drawing.Size(1106, 528);
+            this.splitContainer3.SplitterDistance = 49;
+            this.splitContainer3.TabIndex = 7;
+            // 
+            // ucScanner1
+            // 
+            this.ucScanner1.BaudRate = 9600;
+            this.ucScanner1.DataBits = 8;
+            this.ucScanner1.DelayTimeFile = 1000;
+            this.ucScanner1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ucScanner1.Location = new System.Drawing.Point(0, 0);
+            this.ucScanner1.MaxLength = 16;
+            this.ucScanner1.Name = "ucScanner1";
+            this.ucScanner1.Result = "";
+            this.ucScanner1.SendContent = null;
+            this.ucScanner1.Size = new System.Drawing.Size(1106, 49);
+            this.ucScanner1.Status = "";
+            this.ucScanner1.TabIndex = 0;
+            // 
+            // Forma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1106, 528);
+            this.Controls.Add(this.splitContainer3);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.Name = "Forma";
+            this.Text = "Serial COMM Console by F. Farina Arboccò";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private VTools.ucScanner ucScanner1;
+        private System.Windows.Forms.RichTextBox transmitterRTB;
+        private System.Windows.Forms.RichTextBox receiverRTB;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button clearBtn1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button clearBtn2;
+        private System.Windows.Forms.Button sendFileBtn;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button ping;
+        private System.Windows.Forms.Button resetHostBtn;
+    }
+}
+
---
+++ Forma.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace QuickCOM
+{
+    public partial class Forma : Form
+    {
+        private int lastIndex = 0;
+
+        public void SetControlText(RichTextBox control, string text)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action<RichTextBox, string>(SetControlText), new object[] { control, text });
+            }
+            else
+            {
+                control.AppendText(text);
+            }
+        }
+        private void OpenFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            Application.DoEvents();
+
+            VTools.IucScanner Iscan = ucScanner1;
+
+            this.receiverRTB.ResetText();
+
+            string safefilenamewithExtension = openFileDialog1.SafeFileName;
+            string filepath = openFileDialog1.FileName;
+
+            Iscan.SendFile(ref filepath, ref safefilenamewithExtension);
+        }
+
+        private void SendFileBtn_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        public Forma()
+        {
+            InitializeComponent();
+
+            VTools.IucScanner Iscan = ucScanner1;
+
+            openFileDialog1.FileOk += OpenFileDialog1_FileOk;
+            sendFileBtn.Click += SendFileBtn_Click;
+
+            ping.Click += delegate
+              {
+                  Iscan.SendContent = "<Ping>\n";
+              };
+
+            resetHostBtn.Click += delegate
+              {
+                  receiverRTB.ResetText();
+                  Iscan.SendContent = "<Reset>\n";
+              };
+            EventHandler clear1 = delegate
+              {
+                  transmitterRTB.ResetText();
+                  lastIndex = 0;
+              };
+            EventHandler clear2 = delegate
+            {
+                lastIndex = 0;
+                receiverRTB.ResetText();
+            };
+            this.clearBtn1.Click += clear1;
+            this.clearBtn2.Click += clear2;
+
+       //     Iscan.BaudRate = 9600;
+
+            EventHandler transmit = delegate
+
+            {
+                if (transmitterRTB.Text.Length != 0)
+                {
+                    bool ok = transmitterRTB.Text.Last() == ' ';
+                    ok = ok || transmitterRTB.Text.Last() == '\n';
+                    ok = ok || transmitterRTB.Text.Last() == '.';
+                    if (ok)
+                    {
+                        if (lastIndex < transmitterRTB.Text.Length)
+                        {
+                            string tosend = transmitterRTB.Text.Substring(lastIndex);
+                            Iscan.SendContent = tosend;
+                        }
+                        lastIndex = transmitterRTB.Text.Length ;
+                    }
+                }
+            };
+
+            transmitterRTB.TextChanged += transmit;
+
+            receiverRTB.TextChanged += delegate
+             {
+                 //checks wether the content in the box is a file and arms it.
+               //  Iscan.CheckForFile(receiverRTB.Text);
+             };
+
+            EventHandler handler = delegate
+            {
+                SetControlText(receiverRTB, Iscan.Result);
+            };
+
+            Iscan.MakeScanner(ref handler);
+
+            Iscan.OpenPort = true;
+        }
+    }
+}
